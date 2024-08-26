88c469b700f895af20d8a3a46bc759e3f82dbbfa
lukvmil <millerluke1364@gmail.com>
2024-08-26 15:02:17 -0400

    added slack config

---
+++ slack_config.py
@@ -0,0 +1,17 @@
+initial_users = [
+    "UTER5GXFW",    # Zargham
+    "U06LVP5NMHT",  # Lee
+    "U01672057BN",  # Ellie
+    "U04SDK8LGUX",  # Brooke
+    "U0319V81YGY",  # David
+    "U01G7NSR78S"   # Luke
+]
+
+allowed_channels = [
+    # "C077AFMMFGX",  # koi-testing
+    # "C06LAQNLVNK",  # koi-tank
+    "C04P6EGUYA3",  # attentionecon
+    "C04FU2ZGTT9",  # board
+    "C0175M3GLSU",  # govbase-labs
+    "C06DMGNV7E0",  # koi-pond
+]
